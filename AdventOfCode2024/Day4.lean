62484e02b59aeeba4e5140ef635435444806d455
James Oswald <jamesoswald111@gmail.com>
2024-12-04 03:33:41 -0500

    Day 4

--- AdventOfCode2024/Day4.lean
+++ AdventOfCode2024/Day4.lean
@@ -1,15 +1,21 @@
 
-def sur := [(0,1), (1,0), (1,1), (1,-1), (0,-1), (-1,0), (-1,-1), (-1,1)]
+def sur := #[(0,1), (1,0), (1,1), (1,-1), (0,-1), (-1,0), (-1,-1), (-1,1)]
+def corners : Array (Int × Int) := #[(-1,-1), (-1,1), (1,-1), (1,1)]
+
+def stringToMatrix (s : String) : Array (Array Char) :=
+  ((s.splitOn "\n").map (λ x => x.data.toArray)).toArray
+
+def getD (m : Array (Array Char)) (i j : Int) : Char :=
+  if i >= 0 && j >= 0 then (m.getD i.toNat #[]).getD j.toNat '0' else '0'
+
+-- Part 1
 
 def countXMASAux (m : Array (Array Char)) (i j di dj: Int) (l : List Char := "XMAS".data) : Nat :=
   match l with
   | [] => 1
-  | h :: t =>
-    if i >= 0 && j >= 0 && h == (m.getD i.toNat #[]).getD j.toNat '0' then
-      countXMASAux m (i+di) (j+dj) di dj t
-    else 0
+  | h :: t => if h == getD m i j then countXMASAux m (i+di) (j+dj) di dj t else 0
 
-def countXMAS (m : Array (Array Char)) : Nat :=
+def countXMAS (m : Array (Array Char)) : Nat := Id.run do
   (m.toList.enum.map (λ (i, x) =>
     (x.toList.enum.map (λ (j, _) =>
       (sur.map (λ (di, dj) =>
@@ -17,8 +23,6 @@ def countXMAS (m : Array (Array Char)) : Nat :=
       )).foldl Nat.add 0
     )).foldl Nat.add 0
   )).foldl Nat.add 0
-
-def stringToMatrix (s : String) : Array (Array Char) := ((s.splitOn "\n").map (λ x => x.data.toArray)).toArray
 
 #eval (countXMAS ∘ stringToMatrix) "XMAS"
 
@@ -37,15 +41,12 @@ MAMMMXMMMM
 MXMXAXMASX
 "
 
-def getD (m : Array (Array Char)) (i j : Nat) : Char :=
-  (m.getD i #[]).getD j '0'
-
-def corners : Array (Int × Int) := #[(-1,-1), (-1,1), (1,-1), (1,1)]
+-- Part 2
 
 def isX_MAS (m : Array (Array Char)) (i j : Nat) : Bool :=
-  getD m i j == 'A' &&
-  (corners.map (λ (di, dj) => countXMASAux m (i+di) (j+dj) (-di) (-dj) "MAS".data)).foldl Nat.add 0 == 2
-
+  getD m i j == 'A' && (corners.map (λ (di, dj) =>
+    countXMASAux m (i+di) (j+dj) (-di) (-dj) "MAS".data
+  )).foldl Nat.add 0 == 2
 
 def countX_MAS (m : Array (Array Char)) : Nat :=
   (m.toList.enum.map (λ (i, x) =>
@@ -69,6 +70,6 @@ M.M.M.M.M.
 def main : IO Unit := do
   let input ← IO.FS.readFile "AdventOfCode2024/Day4.txt"
   let result := (countXMAS ∘ stringToMatrix) input
-  IO.print s!"{result}\n"
+  IO.print s!"Part 1 {result}\n"
   let result := (countX_MAS ∘ stringToMatrix) input
-  IO.print s!"{result}\n"
+  IO.print s!"Part 2 {result}\n"
