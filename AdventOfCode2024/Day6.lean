fdc7f52b50998121519371692fc5f1058eb5e8a9
James Oswald <jamesoswald111@gmail.com>
2024-12-06 20:15:27 -0500

    Day 6 first pass

--- AdventOfCode2024/Day6.lean
+++ AdventOfCode2024/Day6.lean
@@ -2,15 +2,21 @@
 import Std.Data.HashSet
 import Std.Data.HashSet
 
+structure GState where
+  pos : Int × Int -- Guard position
+  dir : Int × Int -- x,y direction the guard is facing
+deriving Inhabited, BEq, Hashable
+
+def GState.nextPos (s : GState) : Int × Int := (s.pos.1 + s.dir.1, s.pos.2 + s.dir.2)
+
 structure LabState where
   dim : Nat × Nat -- Width x Height
-  gpos : Int × Int -- Guard position
-  gdir : Int × Int -- x,y direction the guard is facing
+  gs : GState
   objs : Std.HashSet (Int × Int) -- Positions of objects in the lab
 deriving Inhabited
 
-instance : ToString LabState where
-  toString s := s!"{s.dim} {s.gpos} {s.gdir} {s.objs.toList.toString}"
+-- instance : ToString LabState where
+--   toString s := s!"{s.dim} {s.gs.pos} {s.gs.dir} {s.objs.toList.toString}"
 
 -- (1, 0) -> (0, 1) -> (-1, 0) -> (0, -1)
 def rotate : Int × Int -> Int × Int
@@ -31,33 +37,35 @@ def LabState.inBounds (s : LabState) (p : Int × Int) : Bool :=
   0 <= p.1 && p.1 <= s.dim.1 && 0 <= p.2 && p.2 <= s.dim.2
 
 -- Play out the lab state getting the path of the guard with directions
-partial def LabState.run (s : LabState) : List ((Int × Int) × (Int × Int)) :=
-  let next_pos := (s.gpos.1 + s.gdir.1, s.gpos.2 + s.gdir.2);
+partial def LabState.run (s : LabState) : List GState :=
+  let next_pos := s.gs.nextPos
   if s.inBounds next_pos then
     if s.objs.contains next_pos then
-      {s with gdir := rotate s.gdir}.run
+      {s with gs.dir := rotate s.gs.dir}.run
     else
-      {s with gpos := next_pos}.run.append [(s.gpos, s.gdir)]
+      s.gs::{s with gs.pos := next_pos}.run
   else
-    [(s.gpos, s.gdir)]
+    [s.gs]
 
 def parseInput (s : String) : LabState := Id.run do
   let mut objs := Std.HashSet.empty
-  let mut gpos : Int × Int := (0, 0)
-  let mut gdir := (0, 0)
+  let mut gs := ⟨(0, 0), (0, 0)⟩
   let mut dim := (0, 0)
   for ⟨i, line⟩ in s.splitOn "\n"|>.enum do
     dim := {dim with fst := i}
     for ⟨j, c⟩ in line.data.enum do
       dim := {dim with snd := j}
       match c with
-      | '^' | 'v' | '<' | '>' => gpos := (j, i); gdir := rotation c
+      | '^' | 'v' | '<' | '>' => gs := ⟨(j, i), rotation c⟩
       | '#' => objs := objs.insert ((j : Int), (i : Int))
       | _ => ()
-  return {dim, gpos, gdir, objs}
+  return {dim, gs, objs}
+
+def uniquePathPositions (s : LabState) : List (Int × Int) :=
+   s.run.map GState.pos |> Std.HashSet.ofList |>.toList
 
 def part1 (s : String) : Nat :=
-  parseInput s |>.run |>.map Prod.fst |> Std.HashSet.ofList |>.toList.length
+  parseInput s |> uniquePathPositions |>.length
 
 def get_dupes (s : List ((Int × Int) × (Int × Int))) :
 Std.HashMap (Int × Int) (List (Int × Int)) := Id.run do
@@ -66,21 +74,29 @@ Std.HashMap (Int × Int) (List (Int × Int)) := Id.run do
     d := d.insert pos (if H : d.contains pos then (dir::d[pos]) else [dir])
   return d.filter (λ _ v => v.length > 1)
 
--- def isLoopPoint (s : List ((Int × Int) × (Int × Int))) (p : (Int × Int)) : Bool :=
---   let mut d := Std.HashMap.empty
---   for (pos, dir) in s do
---     d := d.insert pos (if H : d.contains pos then (dir::d[pos]) else [dir])
---   d.contains p
+-- Play out a lab state, returning true if the guard enters a loop but false if it exits the lab
+partial def LabState.loops (s : LabState) (v : Std.HashSet GState := {}) : Bool :=
+  if !v.contains s.gs then
+    let next_pos := s.gs.nextPos
+    if s.inBounds next_pos then
+      if s.objs.contains next_pos then
+          {s with gs.dir := rotate s.gs.dir}.loops (v.insert s.gs)
+      else
+        {s with gs.pos := next_pos}.loops (v.insert s.gs)
+    else
+      false
+  else
+    true
 
 def part2 (s : String) : Nat :=
-  let path := parseInput s |>.run
-  dbg_trace s!"{path}"
-  let dupes := get_dupes path
-  dbg_trace s!"{dupes.toList}"
-  s.length
+  let lab := parseInput s
+  uniquePathPositions lab
+  |>.map (λ p => {lab with objs := lab.objs.insert p})
+  |>.filter LabState.loops
+  |>.length
 
 def main : IO Unit := do
-  let input ← IO.FS.readFile "AdventOfCode2024/Day6s.txt"
+  let input ← IO.FS.readFile "AdventOfCode2024/Day6.txt"
   IO.print s!"Part 1: {part1 input}\n"
   IO.print s!"Part 2: {part2 input}\n"
   pure ()
