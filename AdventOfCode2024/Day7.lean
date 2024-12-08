58d43ff26f62e37f4d50cd6a4b9ca600e14335fc
James Oswald <jamesoswald111@gmail.com>
2024-12-08 00:05:22 -0500

    Day 7 part 1 done, part 2 needs more work

--- AdventOfCode2024/Day7.lean
+++ AdventOfCode2024/Day7.lean
@@ -13,20 +13,43 @@ def parseInput (input : String) : List Calibration :=
 def CalibrateWithOps (ops : Nat) : List Nat -> Nat
 | [] => 0
 | [t] => t
-| h::t => dbg_trace s!"Checking {h::t} {ops}"
+| h::t =>
   if (ops % 2 == 0) then
     Nat.mul h (CalibrateWithOps (ops / 2) t)
   else
     Nat.add h (CalibrateWithOps (ops / 2) t)
 
 def Calibration.check (c : Calibration) : Bool :=
-  dbg_trace s!"Checking {c.test} {Nat.pow 2 (c.vs.length - 1)}"
-  List.range (Nat.pow 2 (c.vs.length - 1)) |>.any (CalibrateWithOps · c.vs == c.test)
+  List.range (Nat.pow 2 (c.vs.length - 1)) |>.any (λx =>
+    CalibrateWithOps x c.vs.reverse == c.test)
 
 def part1 (input : String) : Nat :=
   parseInput input |>.filter Calibration.check |>.map (·.test) |>.foldl Nat.add 0
 
+
+def CalibrateWithOps2 (ops : Nat) : List Nat -> Nat
+| [] => 0
+| [t] => t
+| h::t =>
+  if (ops % 3 == 0) then
+    h * (CalibrateWithOps2 (ops / 2) t)
+  else if (ops % 2 == 0) then
+    h + (CalibrateWithOps2 (ops / 2) t)
+  else
+    let ed := (CalibrateWithOps2 (ops / 2) t)
+    let len := ed.log2 / (10 : Nat).log2
+    dbg_trace s!"h: {ed}, len: {len}"
+    h * 10 * len + ed
+
+def Calibration.check2 (c : Calibration) : Bool :=
+  List.range (Nat.pow 3 (c.vs.length - 1)) |>.any (λx =>
+    CalibrateWithOps2 x c.vs.reverse == c.test)
+
+def part2 (input : String) : Nat :=
+  parseInput input |>.filter Calibration.check2 |>.map (·.test) |>.foldl Nat.add 0
+
 def main : IO Unit := do
   let input ← IO.FS.readFile "AdventOfCode2024/Day7s.txt"
   IO.print s!"{part1 input}\n"
+  IO.print s!"{part2 input}\n"
   pure ()
